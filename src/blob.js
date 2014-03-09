e9ca5a4e25ea70bee3c7d250ddaa0077b17d260f
Aleksis Brezas <abresas@gmail.com>
2014-03-09 15:41:11 +0000

    initial commit

---
+++ src/blob.js
@@ -0,0 +1,70 @@
+/** @jsx React.DOM */
+
+var Blob = React.createClass( {
+	areaWidth: null,
+	areaHeight: null,
+	propTypes: {
+		text: React.PropTypes.string.isRequired,
+		pageX: React.PropTypes.number.isRequired,
+		pageY: React.PropTypes.number.isRequired,
+		onClick: React.PropTypes.func,
+		onTextChange: React.PropTypes.func
+	},
+	getDefaultProps: function() {
+		return {
+			onClick: function() {},
+			onTextChange: function() {}
+		};
+	},
+	handleClick: function( event ) {
+		// stop propagation, otherwise a new blob will be created on top of this
+		this.props.onClick( event );
+		event.stopPropagation();
+	},
+	componentDidMount: function() {
+		var node = this.getDOMNode();
+		node.children[ 0 ].focus(); // focus textarea when element ready
+	},
+	measureText: function( text ) {
+		// create a test div to measure the optimal width and height for the textarea
+		// these measurements will later be used to resize textarea
+		var lines = text.split( '\n' );
+		var testNode = document.createElement( 'div' );
+		testNode.className = 'texttest';
+		var textNode = document.createTextNode( '' );
+		lines.forEach( function( line ) {
+			if ( line.length > textNode.nodeValue.length ) {
+				textNode.nodeValue = line.replace( / /g, '_' ); // fix for whitespace not computed correctly
+			}
+		} );
+		testNode.appendChild( textNode );
+		document.body.appendChild( testNode );
+		var width = testNode.clientWidth;
+
+		testNode.innerHTML = text.replace( /\n/g, '<br>_' );
+		var height = testNode.clientHeight;
+		document.body.removeChild( testNode );
+
+		return { width: width, height: height };
+	},
+	render: function() {
+		var divStyle = {
+			left: this.props.pageX,
+			top: this.props.pageY,
+		};
+		var text = this.props.text;
+		var textSize = this.measureText( text );
+		var areaWidth = Math.max( textSize.width + 20, 100 );
+		var areaHeight = Math.max( textSize.height + 5, 16 );
+
+		var textareaStyle = {
+			width: areaWidth + 'px',
+			height: areaHeight + 'px'
+		};
+		return (
+			<div className="blob" style={divStyle} onMouseDown={this.props.onDragStart} onMouseUp={this.props.onDragEnd} onClick={this.handleClick}>
+				<textarea style={textareaStyle} onChange={this.props.onTextChange} onFocus={this.props.onStartEditing} onBlur={this.props.onStopEditing} value={this.props.text}></textarea>
+			</div>
+		);
+	}
+} );
---
+++ src/mindeditor.js
@@ -0,0 +1,108 @@
+/** @jsx React.DOM */
+
+var MindEditor = React.createClass( {
+	dragBlobId: null,
+	getInitialState: function() {
+		window.editor = this;
+		if ( typeof( Storage ) == "undefined" ) {
+			console.log( "Browser does not support localstorage." );
+		}
+		if ( typeof( Storage ) !== "undefined" && localStorage.getItem( "mindEditor.blobs" ) ) {
+			return { 
+				blobs: JSON.parse( localStorage.getItem( "mindEditor.blobs" ) ), 
+				nextBlobId: JSON.parse( localStorage.getItem( "mindEditor.nextBlobId" ) ) 
+			};
+		}
+		else {
+			return { blobs: {}, nextBlobId: 0 }
+		}
+	},
+	componentWillUpdate: function() {
+		if ( typeof( Storage ) !== "undefined" ) {
+			localStorage.setItem( "mindEditor.blobs", JSON.stringify( this.state.blobs ) );
+			localStorage.setItem( "mindEditor.nextBlobId", JSON.stringify( this.state.nextBlobId ) );
+		}
+	},
+	handleClick: function( event ) {
+		console.log( 'mindEditor: click', event.pageX, event.pageY );
+		var id = this.state.nextBlobId;
+		this.state.nextBlobId++;
+		var blob = {
+			x: event.pageX,
+			y: event.pageY,
+			text: '',
+			id: id
+		};
+		this.state.blobs[ id ] = blob;
+		this.setState( {
+			nextBlobId: this.state.nextBlobId,
+			blobs: this.state.blobs
+		} );
+	},
+	handleBlobTextChange: function( blobId, event ) {
+		console.log( 'mindEditor: text change', event.target.value );
+		var blobs = this.state.blobs;
+		var blob = blobs[ blobId ];
+		blob.text = event.target.value;
+		if ( !blob.text ) {
+			delete this.state.blobs[ blobId ];
+		}
+		this.setState( {
+			nextBlobId: this.state.nextBlobId,
+			blobs: this.state.blobs
+		} );
+	},
+	handleStopEditing: function( blobId, event ) {
+		var blob = this.state.blobs[ blobId ];
+		if ( !blob.text ) {
+			delete this.state.blobs[ blobId ];
+		}
+		this.setState( {
+			nextBlobId: this.state.nextBlobId,
+			blobs: this.state.blobs
+		} );
+	},
+	handleDragStart: function( blobId, event ) {
+		console.log( 'mindEditor: drag start', blobId );
+		this.dragBlobId = blobId;
+		var blob = this.state.blobs[ blobId ];
+		this.dragOffsetX = blob.x - event.pageX;
+		this.dragOffsetY = blob.y - event.pageY;
+	},
+	handleMouseMove: function( event ) {
+		if ( this.dragBlobId ) {
+			var blobId = this.dragBlobId;
+			var blob = this.state.blobs[ blobId ];
+			blob.x = event.pageX + this.dragOffsetX;
+			blob.y = event.pageY + this.dragOffsetY;
+			this.setState( {
+				nextBlobId: this.state.nextBlobId,
+				blobs: this.state.blobs
+			} );
+		}
+	},
+	handleDragEnd: function( event ) {
+		this.dragBlobId = null;
+	},
+	render: function() {
+		var divStyle = {
+			width: document.body.scrollWidth,
+			height: document.body.scrollHeight
+		};
+		var blobs = {};
+		for ( var i in this.state.blobs ) {
+			var data = this.state.blobs[ i ];
+			blobs[ 'blob-' + data.id ] = ( 
+				<Blob id={data.id} pageX={data.x} pageY={data.y} text={data.text} 
+					onTextChange={this.handleBlobTextChange.bind( this, data.id )} 
+					onStopEditing={this.handleStopEditing.bind( this, data.id )} 
+					onDragStart={this.handleDragStart.bind( this, data.id )} 
+					onDragEnd={this.handleDragEnd}>
+				</Blob>
+			);
+		}
+		return (
+			<div id="canvas" style={divStyle} onClick={this.handleClick} onMouseMove={this.handleMouseMove}>{blobs}</div>
+		);
+	}
+} );
